d2acb778c3c4a9ed3b36bebd11ffe0604093ce95
NamDo03 <namdo122003@gmail.com>
2024-09-28 17:56:30 +0700

    fix invite modal

--- frontend/src/components/DropdownMenu/DropdownMenu.jsx
+++ frontend/src/components/DropdownMenu/DropdownMenu.jsx
@@ -7,40 +7,38 @@ import InviteMember from "../Modal/InviteMember";
 import useModal from "../../hooks/useModal";
 
 const DropdownMenu = ({ isOpen }) => {
-  const { isOpenModal: isOpenInviteModal, toggleModal: toggleInviteModal } = useModal();
-  if (!isOpen) return null;
-  return (
-    <div
-      className="flex justify-center items-center absolute top-full left-0 w-full"
-    >
-      <div className="w-56 text-xs font-medium text-neutral-400 bg-[#0f0c0d] p-1 rounded">
-        <DropdownItem
-          label="Invite People"
-          onClick={toggleInviteModal}
-          Icon={FaUserPlus}
-          color="text-indigo-400"
-        />
-        <DropdownItem
-          label="Server Settings"
-          onClick={() => console.log("Server Settings clicked")}
-          Icon={IoMdSettings}
-        />
-        <DropdownItem
-          label="Manage Members"
-          onClick={() => console.log("Manage Members clicked")}
-          Icon={IoPeople}
-        />
-        <div className="border-b-[1px] mx-2 my-1 border-neutral-700"></div>
-        <DropdownItem
-          label="Delete Server"
-          onClick={() => console.log("Manage Members clicked")}
-          Icon={FaTrashCan}
-          color="text-red-600"
-        />
+  const { isOpenModal: isOpenInviteModal, toggleModal: toggleInviteModal } =
+    useModal();
+  if (isOpen) return (
+      <div className="flex justify-center items-center absolute top-full left-0 w-full">
+        <div className="w-56 text-xs font-medium text-neutral-400 bg-[#0f0c0d] p-1 rounded">
+          <DropdownItem
+            label="Invite People"
+            onClick={toggleInviteModal}
+            Icon={FaUserPlus}
+            color="text-indigo-400"
+          />
+          <DropdownItem
+            label="Server Settings"
+            onClick={() => console.log("Server Settings clicked")}
+            Icon={IoMdSettings}
+          />
+          <DropdownItem
+            label="Manage Members"
+            onClick={() => console.log("Manage Members clicked")}
+            Icon={IoPeople}
+          />
+          <div className="border-b-[1px] mx-2 my-1 border-neutral-700"></div>
+          <DropdownItem
+            label="Delete Server"
+            onClick={() => console.log("Manage Members clicked")}
+            Icon={FaTrashCan}
+            color="text-red-600"
+          />
+        </div>
+        {isOpenInviteModal && <InviteMember toggleModal={toggleInviteModal} />}
       </div>
-      {isOpenInviteModal&&<InviteMember toggleModal={toggleInviteModal}/>}
-    </div>
-  );
+    );
 };
 
 export default DropdownMenu;
--- frontend/src/components/Modal/InviteMember.jsx
+++ frontend/src/components/Modal/InviteMember.jsx
@@ -4,9 +4,9 @@ import { PiHashBold } from "react-icons/pi";
 import { PiMagnifyingGlass } from "react-icons/pi";
 import { listfriend } from "../../fakeApi";
 
-const InviteMember = ({ toggleModal}) => {
-    const [searchTerm, setSearchTerm] = useState(""); 
-    const filteredFriends = listfriend.filter((friend) =>
+const InviteMember = ({ toggleModal }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredFriends = listfriend.filter((friend) =>
     friend.username.toLowerCase().includes(searchTerm.toLowerCase())
   );
   return (
@@ -14,42 +14,65 @@ const InviteMember = ({ toggleModal}) => {
       <div className="max-w-[500px] w-full bg-primary-1 rounded-md min-w-[400px]">
         <div className="p-4 rounded-t-md">
           <div className="flex justify-between items-center">
-            <h2 className="text-xl text-zinc-200">Invite friends to join your server</h2>
+            <h2 className="text-xl text-zinc-200">
+              Invite friends to join your server
+            </h2>
             <button
               onClick={toggleModal}
               className="text-zinc-400 hover:text-zinc-200"
             >
               <IoClose size={24} />
             </button>
+          </div>
+          <div>
+            <div
+              className={` flex items-center py-[3px] rounded-[4px] my-3 bg-zinc-700":"bg-primary-2"}`}
+            >
+              <PiHashBold size={20} className="text-zinc-400" />
+              <p className="text-zinc-400 text-xs ml-2">Channel name</p>
             </div>
-            <div>
-                <div className={` flex items-center  py-[3px]  rounded-[4px] mb-1 bg-zinc-700":"bg-primary-2"}`}>
-                    <PiHashBold size={20} className="text-zinc-400"/>
-                    <p className="text-zinc-400 text-xs mt-4 mb-2 ml-2">
-                        Channel name
-                    </p>
-                </div>
-                    <div className="flex items-center relative"> 
-                    <input  value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} type="text" className="w-full bg-primary-3 py-2 rounded-sm px-3" placeholder="Search for friends"/>
-                    <PiMagnifyingGlass size={24} className="text-zinc-400 absolute right-2"/>
-                    </div>    
-
+            <div className="flex items-center relative">
+              <input
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                type="text"
+                className="w-full bg-primary-3 py-2 rounded-sm px-3 outline-none"
+                placeholder="Search for friends"
+              />
+              <PiMagnifyingGlass
+                size={24}
+                className="text-zinc-400 absolute right-2"
+              />
             </div>
-        </div> 
-        <div className="h-[1px] mt-5 bg-zinc-900"/>        
-                <div className="m-4 overflow-y-scroll max-h-[250px]"> 
-                    {filteredFriends.map((friend)=>(
-                        <div key={friend.id} className="flex justify-between items-center px-2 py-1 group hover:bg-zinc-700">
-                            <div className="flex items-center gap-3"> 
-                                <img className="w-9 h-9 object-cover rounded-full" src={friend.avatarUrl} alt="" />
-                                <p className="text-zinc-300">{friend.username}</p>
-                            </div>
-                            <button className="border border-green px-4 py-2 rounded-[4px] hover:bg-green group-hover:bg-green"> 
-                                Invite
-                            </button>
-                        </div>
-                    ))}
+          </div>
+        </div>
+        <div className="h-[1px] mt-5 bg-zinc-900" />
+        <div className="m-4 overflow-y-auto max-h-[200px]">
+          {filteredFriends.length > 0 ? (
+            filteredFriends.map((friend) => (
+              <div
+                key={friend.id}
+                className="flex justify-between items-center px-2 py-1 group hover:bg-zinc-700"
+              >
+                <div className="flex items-center gap-3">
+                  <img
+                    className="w-9 h-9 object-cover rounded-full"
+                    src={friend.avatarUrl}
+                    alt={friend.username}
+                  />
+                  <p className="text-zinc-300">{friend.username}</p>
                 </div>
+                <button className="border border-green px-4 py-2 rounded-[4px] hover:bg-green group-hover:bg-green">
+                  Invite
+                </button>
+              </div>
+            ))
+          ) : (
+            <div className="text-zinc-400 text-center h-[200px] flex items-center justify-center uppercase font-semibold">
+              No results found
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
