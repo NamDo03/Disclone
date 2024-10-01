9541bdf85ee9cab6cd890b75ab2fcc29787280d8
NamDo03 <namdo122003@gmail.com>
2024-10-01 21:06:08 +0700

    edit server/channel modal

--- frontend/src/components/ServerSideBar/ServerHeader.jsx
+++ frontend/src/components/ServerSideBar/ServerHeader.jsx
@@ -2,18 +2,16 @@ import React, { useState } from "react";
 import { FaChevronDown } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
-import useClickOutside from "../../hooks/useClickOutside";
 
 const ServerHeader = ({ name }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const ref = useClickOutside(() => setIsDropdownOpen(false));
 
   const toggleDropdown = () => {
     setIsDropdownOpen((prev) => !prev);
   };
 
   return (
-    <div ref={ref} className="relative">
+    <div className="relative">
       <div
         onClick={toggleDropdown}
         className="w-full text-md font-semibold px-3 flex items-center justify-between h-12 
@@ -23,7 +21,7 @@ const ServerHeader = ({ name }) => {
         {isDropdownOpen ? <IoClose size={20} /> : <FaChevronDown />}
       </div>
       {isDropdownOpen && (
-        <DropdownMenu isOpen={isDropdownOpen} setIsOpen={setIsDropdownOpen} />
+        <DropdownMenu />
       )}
     </div>
   );
