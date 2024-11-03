86fd9086d9e5c6b014444b878695bc3b3f56f8f6
NamDo03 <namdo122003@gmail.com>
2024-11-03 15:53:20 +0700

    bad words

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const ProtectedRoute = ({ children }) => {
   const currentUser = useSelector((state) => state.user.currentUser);
+  const token = Cookies.get("token");;
   const location = useLocation();
-  if (!currentUser) {
+  if (!currentUser || !token) {
     return <Navigate to="/sign-in" state={{ from: location }} replace />;
   }
   return children;
